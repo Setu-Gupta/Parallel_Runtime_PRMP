2aa1c5ad97f9a0d95c33cf82db0654ac61f2d573
madhur19171 <madhur19171@iiitd.ac.in>
2022-09-30 23:16:45 +0530

    First draft of Header and Library file.

--- src/argolib.c
+++ src/argolib.c
@@ -1,6 +1,125 @@
 #include <stdio.h>
- 
+#include "../include/argolib.h"
+
+int num_xstreams;
+int num_threads;
+
+void argolib_init(int argc, char **argv)
+{
+	int is_randws = 0;
+	// Minimum size Execution Streams and Threads when taken from user
+	if (num_xstreams <= 0)
+		num_xstreams = 1;
+	if (num_threads <= 0)
+		num_threads = 1;
+
+	*xstreams = (ABT_xstream *)malloc(sizeof(ABT_xstream) * num_xstreams);
+	*pools = (ABT_pool *)malloc(sizeof(ABT_pool) * num_xstreams);
+	*scheds = (ABT_sched *)malloc(sizeof(ABT_sched) * num_xstreams);
+
+	*threads = (ABT_thread *)malloc(sizeof(ABT_thread) * num_threads);
+
+	ABT_init(argc, argv);
+
+	/* Set up a primary execution stream. */
+	ABT_xstream_self(&xstreams[0]);
+
+	/* Create pools. */
+	for (int i = 0; i < num_xstreams; i++)
+	{
+		if (is_randws)
+		{
+			ABT_pool_create_basic(ABT_POOL_RANDWS, ABT_POOL_ACCESS_MPMC,
+								  ABT_TRUE, &pools[i]);
+		}
+		else
+		{
+			ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_MPMC, ABT_TRUE,
+								  &pools[i]);
+		}
+	}
+
+	/* Create schedulers. */
+	for (int i = 0; i < num_xstreams; i++)
+	{
+		ABT_pool *tmp = (ABT_pool *)malloc(sizeof(ABT_pool) * num_xstreams);
+		for (int j = 0; j < num_xstreams; j++)
+		{
+			tmp[j] = pools[(i + j) % num_xstreams];
+		}
+		//?Difference between ABT_POOL_RANDWS and ABT_SCHED_RANDWS?
+		ABT_sched_create_basic(ABT_SCHED_DEFAULT, num_xstreams, tmp,
+							   ABT_SCHED_CONFIG_NULL, &scheds[i]);
+		free(tmp);
+	}
+
+	// Set the scheduler for the primary execution stream
+	ABT_xstream_set_main_sched(xstreams[0], scheds[0]);
+
+	/* Create secondary execution streams. */
+	for (int i = 1; i < num_xstreams; i++)
+	{
+		ABT_xstream_create(scheds[i], &xstreams[i]);
+	}
+}
+
+Task_handle *argolib_fork(fork_t fptr, void *args)
+{
+	/** Create ULTs.
+	 * The pool associated with this thread is same as the pool of the caller.
+	 * thread_pointer will be returned to the caller hence defined static.
+	 * Preferably, the caller should pass a thread_arg_t pointer
+	*/
+	static Task_handle* thread_pointer;
+
+	int rank;
+	ABT_xstream_self_rank(&rank); // Gets the pool index of the calling pool
+	ABT_pool target_pool = pools[rank];
+	// When should we use ABT_thread_create_to ?
+	ABT_thread_create(target_pool, fptr, args,
+					  ABT_THREAD_ATTR_NULL, thread_pointer);
+	
+	return thread_pointer;
+}
+
+void argolib_kernel(fork_t fptr, void *args){
+	/**TODO
+	 * Print Statistics
+	*/
+	Task_handle** kernel_task;
+	*kernel_task = argolib_fork(fptr, args);
+	argolib_join(kernel_task, 1);
+}
+
+void argolib_join(Task_handle **list, int size)
+{
+	// First join all the threads
+	// ABT_thread_join might not be needed. Confirm!
+	for (int i = 0; i < size; i++)
+	{
+		ABT_thread_join((*list[i]));
+	}
+
+	// Free all the threads
+	for (int i = 0; i < size; i++)
+	{
+		ABT_thread_free(list[i]);
+	}
+}
+
+void argolib_finalize()
+{
+	// Finalize argobots
+	ABT_finalize();
+
+	// Free allocated memory
+	free(xstreams);
+	free(pools);
+	free(scheds);
+	free(threads);
+}
+
 void test(void)
 {
-        printf("Hello, I am a shared library\n");
+	printf("Hello, I am a shared library\n");
 }
---
+++ tests/fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <argolib.h>
+
+typedef struct {
+    int n;
+    int ret;
+} fibonacci_arg_t;
+
+
+void fib(fibonacci_arg_t* fib_arg)
+{
+    if (fib_arg->n < 2){
+        fib_arg->ret = fib_arg->n;
+    }
+
+    fibonacci_arg_t *x, *y;
+    x = (fibonacci_arg_t*) malloc(sizeof(fibonacci_arg_t));
+    y = (fibonacci_arg_t*) malloc(sizeof(fibonacci_arg_t));
+    x->n = fib_arg->n - 1;
+    y->n = fib_arg->n - 2;
+
+    Task_handle* task1 = argolib_fork((fork_t)fib, (void*)x);
+    Task_handle* task2 = argolib_fork((fork_t)fib, (void*)y);
+    Task_handle* task_pair[2] = {task1, task2};
+    argolib_join(task_pair, 2);
+
+    fib_arg->ret = x->ret + y->ret;
+}
+
+
+int main(int argc, char **argv) {
+    argolib_init(argc, argv);
+    int result;
+    fibonacci_arg_t* arg;
+    arg = (fibonacci_arg_t*) malloc(sizeof(fibonacci_arg_t));
+    arg->n = 40;
+    arg->ret = 0;
+    argolib_kernel((fork_t)fib, (void*)arg);
+    result = arg->ret;
+    printf("Fib(40) = %d\n", result);
+    argolib_finalize();
+    return 0;
+}
