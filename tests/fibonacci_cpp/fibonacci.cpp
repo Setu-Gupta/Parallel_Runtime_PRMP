00790d1f9f72586594cbd25cb3b1aa11824e70bb
Setu Gupta <setu.gupta.2020@gmail.com>
2022-10-06 23:23:00 +0530

    Debug statements added to C++ fibonacci
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   tests/fibonacci_cpp/fibonacci.cpp

--- tests/fibonacci_cpp/fibonacci.cpp
+++ tests/fibonacci_cpp/fibonacci.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <argolib.hpp>
+#include <mutex>
 
-int fib(int n) {
-        if(n<2) return n;
+std::mutex m;
+
+int fib(int n)
+{
+        if(n < 2)
+                return n;
         int x, y;
         Task_handle* task1 = argolib::fork([&]() { x = fib(n-1); });
         Task_handle* task2 = argolib::fork([&]() { y = fib(n-2); });
         argolib::join(task1, task2);
-        return x+y;
+        m.lock();
+        std::cout << "n:" << n << " x:" << x << " y:" << y << std::endl;
+        m.unlock();
+        return x + y;
 }
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
         argolib::init(argc, argv);
         int result;
         argolib::kernel([&]() {
-                        result = fib(40);
+                        result = fib(4);
+                        m.lock();
+                        std::cout << "result: " << result << std::endl;
+                        m.unlock();
                         });
-        std::cout << "Fib(40) = "<<result<<std::endl;
+        std::cout << "Fib(4) = " << result << std::endl;
         argolib::finalize();
         return 0;
 }
