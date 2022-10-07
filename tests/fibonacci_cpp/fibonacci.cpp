396054bb2226b3e65e17dd7215eff438034765e6
madhur19171 <madhur19171@iiitd.ac.in>
2022-10-07 18:05:05 +0530

    Giving up on c++ test
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   tests/fibonacci_cpp/fibonacci.cpp

--- tests/fibonacci_cpp/fibonacci.cpp
+++ tests/fibonacci_cpp/fibonacci.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
 #include <argolib.hpp>
-#include <mutex>
-
-std::mutex m;
 
 int fib(int n)
 {
-        if(n < 2)
+	if(n < 2)
                 return n;
-        int x, y;
-        Task_handle* task1 = argolib::fork([&]() { x = fib(n-1);
-                        m.lock();
-                        std::cout << "for " << n-1 << " x:" << x << std::endl;
-                        m.unlock();
-                        });
-        Task_handle* task2 = argolib::fork([&]() { y = fib(n-2);
-                        m.lock();
-                        std::cout << "for " << n-2 << " y:" << y << std::endl;
-                        m.unlock();
-                        });
+        int x = 0, y = 0;
+        Task_handle* task1 = argolib::fork([&]() { x = fib(n-1);});
+        Task_handle* task2 = argolib::fork([&]() { y = fib(n-2);});
         argolib::join(task1, task2);
-        m.lock();
-        std::cout << "n:" << n << " x:" << x << " y:" << y << std::endl;
-        m.unlock();
-        return x + y;
+	return x + y;
 }
+
 int main(int argc, char **argv)
 {
         argolib::init(argc, argv);
         int result;
-        argolib::kernel([&]() {
-                        result = fib(4);
-                        m.lock();
-                        std::cout << "result: " << result << std::endl;
-                        m.unlock();
-                        });
-        std::cout << "Fib(4) = " << result << std::endl;
+        argolib::kernel([&]() {result = fib(40);});
+        std::cout << "Fib(40) = " << result << std::endl;
         argolib::finalize();
         return 0;
 }
