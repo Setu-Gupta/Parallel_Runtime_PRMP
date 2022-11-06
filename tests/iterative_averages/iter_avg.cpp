af617c96f7b6360ba01065f7947fb1174dcb675b
Setu Gupta <setu.gupta.2020@gmail.com>
2022-11-06 16:01:09 +0530

    Removed print statements and add iterative averaging
    On branch trace_replay
    Your branch is up to date with 'origin/trace_replay'.
    
    Changes to be committed:
    	modified:   src/argolib.c
    	new file:   tests/iterative_averages/Makefile
    	new file:   tests/iterative_averages/bin/debug/.gitignore
    	new file:   tests/iterative_averages/bin/release/.gitignore
    	new file:   tests/iterative_averages/build/debug/.gitignore
    	new file:   tests/iterative_averages/build/release/.gitignore
    	new file:   tests/iterative_averages/iter_avg.cpp

---
+++ tests/iterative_averages/iter_avg.cpp
@@ -0,0 +1,67 @@
+#include <cstdlib>
+#include <iostream>
+#include <argolib.hpp>
+
+#define ITERS   10
+#define SIZE    10
+#define MAX_VAL 100
+
+#define THRESHOLD       5
+void iter_avg(int* arr, int size)
+{
+        if(size < THRESHOLD)
+        {
+                for(int i = 0; i < size; i++)
+                {
+                        int cur =  arr[i];
+                        int prev = i-1 >= 0 ? arr[i-1] : 0;
+                        int next = i+1 < size ? arr[i+1] : 0;
+                        arr[i] = (prev + cur + next)/3;
+                }
+        }
+        else
+        {
+                int mid = size/2;
+                Task_handle* task = argolib::fork([=]()
+                                                {
+                                                        iter_avg(arr, mid);
+                                                });
+                iter_avg(arr+mid, size-mid);
+                argolib::join(task);
+        }
+}
+
+int main(int argc, char **argv)
+{
+        // Create an array of size SIZE and populate it randomly
+        int *arr = (int *)calloc(SIZE, sizeof(int));
+        for(int i = 0; i < SIZE; i++)
+                arr[i] = rand() % MAX_VAL;
+
+        // Print out the array
+        std::cout << "Before averaging:" << std::endl;
+        for(int i = 0; i < SIZE; i++)
+                std::cout << arr[i] << " ";
+        std::cout << std::endl; 
+
+        // Run parallel interative averaging
+        argolib::init(argc, argv);
+        for(int i = 0; i < ITERS; i++)
+        {
+                argolib::start_tracing();
+                iter_avg(arr, SIZE);
+                argolib::stop_tracing();
+        }
+        argolib::finalize();
+        
+        // Print out the array after averaging
+        std::cout << "After averaging:" << std::endl;
+        for(int i = 0; i < SIZE; i++)
+                std::cout << arr[i] << " ";
+        std::cout << std::endl; 
+
+        // Free the array
+        free(arr);
+
+        return 0;
+}
