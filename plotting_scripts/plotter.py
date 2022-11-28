669c195f75e199683c5e45f9ab1010fb3bb3296d
Setu Gupta <setu.gupta.2020@gmail.com>
2022-11-28 21:33:49 +0530

    Added plotting script for M3 evaluation
    On branch power_optimization
    Your branch is up to date with 'origin/power_optimization'.
    
    Changes to be committed:
    	new file:   plotting_scripts/.gitignore
    	new file:   plotting_scripts/plotter.py

---
+++ plotting_scripts/plotter.py
@@ -0,0 +1,17 @@
+import sys
+from matplotlib import pyplot as plt
+
+vals = []
+with open(sys.argv[1], 'r') as infile:
+    for line in infile.readlines():
+        line = line.strip()
+        if 'e-' in line:
+            vals.append(float(line))
+
+plt.title("Joules per instruction (JPI) v/s Execution time")
+plt.xlabel("Sample number")
+plt.ylabel("JPI")
+plt.autoscale(enable=True, axis='both', tight=True)
+plt.grid()
+plt.plot(vals)
+plt.savefig("plot.png")
