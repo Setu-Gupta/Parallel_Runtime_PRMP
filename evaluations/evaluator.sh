351b3faeb54d02f333880f0561113a5e6b160864
Setu Gupta <setu.gupta.2020@gmail.com>
2022-12-04 03:34:44 +0530

    Added automatic evaluation script
    On branch evaluation
    Your branch is up to date with 'origin/evaluation'.
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   evaluator.sh

---
+++ evaluations/evaluator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo ================================SEQUENTIAL=========================================
+for benchmark in ./seq*
+do
+        cd $benchmark
+        make clean > /dev/null
+        make > /dev/null
+        cd bin/release
+        bin=$(ls)
+        ./$bin
+        cd ../..
+        make clean > /dev/null
+        cd ..
+done
+echo ================================END=========================================
+echo ""
+echo ""
+
+echo ================================PARALLEL=========================================
+for benchmark in ./argolib*
+do
+        cd $benchmark
+        for i in Baseline_Argolib M1_sub M1_opt M2_sub M2_opt
+        do
+                echo ================================$i=========================================
+                cd ../..
+                git checkout $i 2>/dev/null > /dev/null
+                make clean > /dev/null
+                make > /dev/null
+                git checkout evaluation 2>/dev/null > /dev/null
+                cd evaluations/$benchmark
+                make clean > /dev/null
+                make > /dev/null
+                cd bin/release
+                bin=$(ls)
+                ARGOLIB_WORKERS=20 ./$bin
+                cd ../..
+                make clean > /dev/null
+                echo ++++++++++++++++++++++++++++++++$i+++++++++++++++++++++++++++++++++++++++++
+        done
+
+        cd ../..
+        make clean > /dev/null
+        cd evaluations
+        echo ""
+done
+echo ================================END=========================================
